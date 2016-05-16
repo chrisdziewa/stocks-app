4f0db4dbcd8690f92acbdb97d601c7a0852d865f
Chris Dziewa <chris.dziewa@gmail.com>
2016-05-16 16:21:12 -0500

    Update production keys

--- app/config/index.js
+++ app/config/index.js
@@ -4,8 +4,7 @@ if (process.env.NODE_ENV === 'production') {
   // Offer production stage env vars
   module.exports = {
     host: process.env.host || "",
-    dbURI: process.env.dbURI,
-    sessionSecret: process.env.sessionSecret,
+    dbURI: `mongodb://${process.env.dbUser}:${process.env.dbPassword}@ds023902.mlab.com:23902/stocks-app`
   }
 } else {
   module.exports = require('./development.json');
