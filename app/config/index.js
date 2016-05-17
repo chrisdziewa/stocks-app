be73d99589e9f5cff03891130e72fa8556b2c586
Chris Dziewa <chris.dziewa@gmail.com>
2016-05-16 22:37:33 -0500

    Fix production config

--- app/config/index.js
+++ app/config/index.js
@@ -4,7 +4,10 @@ if (process.env.NODE_ENV === 'production') {
   // Offer production stage env vars
   module.exports = {
     host: process.env.host || "",
-    dbURI: `mongodb://${process.env.dbUser}:${process.env.dbPassword}@ds023902.mlab.com:23902/stocks-app`
+    dbURI: `mongodb://${process.env.dbUser}:${process.env.dbPassword}@ds023902.mlab.com:23902/stocks-app`,
+    quandl: {
+      api_key: process.env.api_key
+    }
   }
 } else {
   module.exports = require('./development.json');
