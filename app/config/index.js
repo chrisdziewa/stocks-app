94fa1560f2f8eba648d2487c6b6c025ebc0c480e
Chris Dziewa <chris.dziewa@gmail.com>
2016-05-16 23:14:30 -0500

    Remove quotes

--- app/config/index.js
+++ app/config/index.js
@@ -3,10 +3,10 @@
 if (process.env.NODE_ENV === 'production') {
   // Offer production stage env vars
   module.exports = {
-    "host": process.env.host || "",
-    "dbURI": `mongodb://${process.env.dbUser}:${process.env.dbPassword}@ds023902.mlab.com:23902/stocks-app`,
-    "quandl": {
-      "api_key": process.env.apiKey
+    host: process.env.host || "",
+    dbURI: `mongodb://${process.env.dbUser}:${process.env.dbPassword}@ds023902.mlab.com:23902/stocks-app`,
+    quandl: {
+      api_key: process.env.apiKey
     }
   }
 } else {
