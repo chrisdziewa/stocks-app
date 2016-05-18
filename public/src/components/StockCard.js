e4ccf9762cf86d513e05bd122479c4b3962d742f
Chris Dziewa <chris.dziewa@gmail.com>
2016-05-18 17:08:39 -0500

    Add basic responsive styling

--- public/src/components/StockCard.js
+++ public/src/components/StockCard.js
@@ -12,14 +12,16 @@ class StockCard extends Component {
     let fullName = this.props.fullName.substr(0, endString + 1);
     return (
       <div className="stock-card">
-        <p>{this.props.symbol}</p>
-        <p>{fullName}</p>
-        <button
-          className="close"
-          onClick={this.handleClick.bind(this)}
-        >
-          X
-        </button>
+        <div className="stock-card__inner">
+          <p>{this.props.symbol}</p>
+          <p>{fullName}</p>
+          <button
+            className="close"
+            onClick={this.handleClick.bind(this)}
+          >
+            X
+          </button>
+        </div>
       </div>
     );
   }
